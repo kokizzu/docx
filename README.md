d1ed8a8a9717b8e26de7b3abe2e6874f96274aa3
Kiswono Prayogo <kiswono@gmail.com>
2016-06-15 09:31:33 +0700

    added ReplaceContent, ReplaceHeader, ReplaceFooter, ReplaceContentRaw

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The following constitutes the bare minimum required to replace text in DOCX docu
 ``` go 
 
 import (
-	"github.com/nguyenthenguyen/docx"
+	"github.com/kokizzu/docx"
 )
 
 func main() {
@@ -13,13 +13,13 @@ func main() {
 		panic(err)
 	}
 	docx1 := r.Editable()
-	docx1.Replace("old_1_1", "new_1_1", -1)
-	docx1.Replace("old_1_2", "new_1_2", -1)
+	docx1.ReplaceContent("old_1_1", "new_1_1", -1)
+	docx1.ReplaceContentRaw("old_1_2", "new_1_2", -1) // replace newline with `</w:t><w:br/><w:t>``
 	docx1.WriteToFile("./new_result_1.docx")
 
 	docx2 := r.Editable()
-	docx2.Replace("old_2_1", "new_2_1", -1)
-	docx2.Replace("old_2_2", "new_2_2", -1)
+	docx2.ReplaceHeader("old_2_1", "new_2_1", -1)
+	docx2.ReplaceFooter("old_2_2", "new_2_2", -1)
 	docx2.WriteToFile("./new_result_2.docx")
 
 	r.Close()
--- docx.go
+++ docx.go
@@ -1,26 +1,30 @@
 package docx
 
 import (
-	"archive/zip"
-	"bufio"
-	"bytes"
-	"encoding/xml"
-	"errors"
-	"io"
-	"io/ioutil"
-	"os"
-	"strings"
+	`archive/zip`
+	`bufio`
+	`bytes`
+	`encoding/xml`
+	`errors`
+	`io`
+	`io/ioutil`
+	`os`
+	`strings`
 )
 
 type ReplaceDocx struct {
 	zipReader *zip.ReadCloser
 	content   string
+	header    string
+	footer    string
 }
 
 func (r *ReplaceDocx) Editable() *Docx {
 	return &Docx{
 		files:   r.zipReader.File,
 		content: r.content,
+		header: r.header,
+		footer: r.footer,
 	}
 }
 
@@ -31,9 +35,25 @@ func (r *ReplaceDocx) Close() error {
 type Docx struct {
 	files   []*zip.File
 	content string
+	header  string
+	footer  string
 }
 
-func (d *Docx) Replace(oldString string, newString string, num int) (err error) {
+func (d *Docx) ReplaceContentRaw(oldString string, newString string, num int) (err error) {
+	oldString, err = encode(oldString)
+	if err != nil {
+		return err
+	}
+	newString, err = encode(newString)
+	if err != nil {
+		return err
+	}
+	newString = strings.Replace(newString,`&#xA;`,`</w:t><w:br/><w:t>`,-1)
+	d.content = strings.Replace(d.content, oldString, newString, num)
+	return nil
+}
+
+func (d *Docx) ReplaceContent(oldString string, newString string, num int) (err error) {
 	oldString, err = encode(oldString)
 	if err != nil {
 		return err
@@ -43,6 +63,33 @@ func (d *Docx) Replace(oldString string, newString string, num int) (err error) 
 		return err
 	}
 	d.content = strings.Replace(d.content, oldString, newString, num)
+	return nil
+}
+
+func (d *Docx) ReplaceHeader(oldString string, newString string, num int) (err error) {
+	oldString, err = encode(oldString)
+	if err != nil {
+		return err
+	}
+	newString, err = encode(newString)
+	if err != nil {
+		return err
+	}
+	d.header = strings.Replace(d.header, oldString, newString, num)
+
+	return nil
+}
+
+func (d *Docx) ReplaceFooter(oldString string, newString string, num int) (err error) {
+	oldString, err = encode(oldString)
+	if err != nil {
+		return err
+	}
+	newString, err = encode(newString)
+	if err != nil {
+		return err
+	}
+	d.footer = strings.Replace(d.footer, oldString, newString, num)
 
 	return nil
 }
@@ -72,9 +119,14 @@ func (d *Docx) Write(ioWriter io.Writer) (err error) {
 		if err != nil {
 			return err
 		}
-		if file.Name == "word/document.xml" {
+		switch file.Name {
+		case `word/document.xml`:
 			writer.Write([]byte(d.content))
-		} else {
+		case `word/header1.xml`:
+			writer.Write([]byte(d.header))
+		case `word/footer1.xml`:
+			writer.Write([]byte(d.footer))
+		default:
 			writer.Write(streamToByte(readCloser))
 		}
 	}
@@ -82,53 +134,46 @@ func (d *Docx) Write(ioWriter io.Writer) (err error) {
 	return
 }
 
-func ReadDocxFile(path string) (*ReplaceDocx, error) {
-	reader, err := zip.OpenReader(path)
+func ReadDocxFile(cpath string) (*ReplaceDocx, error) {
+	reader, err := zip.OpenReader(cpath)
 	if err != nil {
 		return nil, err
 	}
-	content, err := readText(reader.File)
+	content, header, footer, err := readText(reader.File)
 	if err != nil {
 		return nil, err
 	}
 
-	return &ReplaceDocx{zipReader: reader, content: content}, nil
+	return &ReplaceDocx{zipReader: reader, content: content, header: header, footer: footer}, nil
 }
 
-func readText(files []*zip.File) (text string, err error) {
-	var documentFile *zip.File
-	documentFile, err = retrieveWordDoc(files)
-	if err != nil {
-		return text, err
-	}
-	var documentReader io.ReadCloser
-	documentReader, err = documentFile.Open()
-	if err != nil {
-		return text, err
-	}
-
-	text, err = wordDocToString(documentReader)
+func readText(files []*zip.File) (content, header, footer string, err error) {
+	content, err = retrieveWordXml(files, `document.xml`)
+	header, err = retrieveWordXml(files, `header1.xml`)
+	footer, err = retrieveWordXml(files,`footer1.xml`)
 	return
 }
 
-func wordDocToString(reader io.Reader) (string, error) {
-	b, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
-}
 
-func retrieveWordDoc(files []*zip.File) (file *zip.File, err error) {
+func retrieveWordXml(files []*zip.File, name string) (string, error) {
+	var file *zip.File
 	for _, f := range files {
-		if f.Name == "word/document.xml" {
+		if f.Name == `word/` + name {
 			file = f
 		}
 	}
 	if file == nil {
-		err = errors.New("document.xml file not found")
+		return ``, errors.New(`document.xml file not found`)
 	}
-	return
+	documentReader, err := file.Open()
+	if err != nil {
+		return ``, err
+	}
+	b, err := ioutil.ReadAll(documentReader)
+	if err != nil {
+		return ``, err
+	}
+	return string(b), nil
 }
 
 func streamToByte(stream io.Reader) []byte {
@@ -143,5 +188,5 @@ func encode(s string) (string, error) {
 	if err := enc.Encode(s); err != nil {
 		return s, err
 	}
-	return strings.Replace(strings.Replace(b.String(), "<string>", "", 1), "</string>", "", 1), nil
+	return strings.Replace(strings.Replace(b.String(), `<string>`, ``, 1), `</string>`, ``, 1), nil
 }
